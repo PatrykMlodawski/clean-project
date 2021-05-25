293fbe2f0ffdebecba9b6491fee415d678828b22
PatrykMlodawski <pk.mlodawski@gmail.com>
2021-05-25 21:51:41 +0200

    Added new features

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,30 @@
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper/core';
+
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
 const btns = document.querySelector('.menu-list');
 
 function easeInOutCubic(t, b, c, d) {
@@ -11,10 +38,10 @@ function smoothScroll(event) {
   event.preventDefault();
   const targetId = event.target.getAttribute('href');
   let targetPosition = document.querySelector(targetId).offsetTop;
-  if (window.innerWidth < 1600) {
+  if (window.innerWidth < 800) {
     targetPosition -= 60;
-  } else if (window.innerWidth >= 1600) {
-    targetPosition -= 100;
+  } else if (window.innerWidth >= 800) {
+    targetPosition -= 80;
   }
   const startPosition = window.pageYOffset;
   const distance = targetPosition - startPosition;
